b5e3b8ac188d35da16546ac5b2904bb776784509
paulamorales089 <paitomorales03@gmail.com>
2021-04-14 21:15:29 -0500

    Start with classes

---
+++ src/view/principal.java
@@ -0,0 +1,38 @@
+package view;
+import processing.core.PApplet;
+	// paso 0
+public class principal extends PApplet{
+
+	public static void main(String[] args) {	
+		
+		PApplet.main("principal");
+	}
+	
+	@Override
+	public void settings() {
+		size(500,500);
+	}
+	
+	@Override
+	public void setup() {
+		}
+	
+	@Override
+	public void draw() {
+		background(255);
+		System.out.println(mouseX + ","+ mouseY);
+		
+		
+		
+		
+		
+		
+
+	
+		
+		
+		
+		
+		}
+	
+}
